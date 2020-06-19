bf943c70b870ed56568f56b3e67b083aea624922
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2020-06-19 17:17:42 +0530

    Made navbar collapsable automatically

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+var isActive = false;
 
 toggleButton.addEventListener('click', () => {
-	navbarLinks.classList.toggle('active')
-	console.log("click recorded");
+	isActive = !isActive;
+	navbarLinks.classList.toggle('active');
 })
+
+document.body.addEventListener('click', () => {
+	if(isActive) {
+		navbarLinks.classList.toggle('active');
+		isActive = !isActive;
+	}
+}, true);
