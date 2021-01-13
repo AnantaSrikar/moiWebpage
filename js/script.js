091faec43b75500a55afdbfc55662bcf5ddccd63
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-01-13 12:47:45 +0530

    Added dynamic last updated

--- js/script.js
+++ js/script.js
@@ -19,3 +19,37 @@ document.body.addEventListener('click', () => {
 	}
 	
 }, true);
+
+// Uses GitHub API to get the last commit done on my repo
+function get_last_updated() {
+
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.open('GET','https://api.github.com/repos/AnantaSrikar/anantasrikar.github.io/commits', true);
+	xhttp.setRequestHeader("Content-Type", "application/json");
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			
+			// Getting the response
+			var response = JSON.parse(this.responseText);
+
+			// Getting the last commit details
+			var date = new Date(response[0]['commit']['author']['date']);
+			
+			var update_date_str = date.getDate() + '-' + date.toLocaleString('default', { month: 'short' }) + '-' + date.getFullYear();
+			
+			// Getting the footer element to modify dynamically
+			var footer = document.getElementsByClassName("row")[0].getElementsByTagName("p")[0];
+			
+			footer.innerHTML += " | Last updated: " + update_date_str;
+		}
+	};
+	
+	xhttp.send();
+}
+
+// Funtion that gets called everytime a page is loaded
+window.onload = function() {
+	get_last_updated();
+  };
